2b33515f4071e6f54217b90da011cbea4e06080e
Tsurkanov Mykhailo <mikhail.tsurkanov@overdose.digital>
2024-10-02 11:07:05 +0300

    added error handling fpr the best result fetching request

--- src/components/BestResults.jsx
+++ src/components/BestResults.jsx
@@ -1,30 +1,47 @@
 import {useContext, useEffect, useState} from "react";
 import {GameContext} from "../store/game-context.jsx";
+import ErrorBlock from "./ErrorBlock.jsx";
 
 export function BestResults() {
     const [bestResult, setBestResult] = useState([]);
-    const {size, gameCount} =  useContext(GameContext);
+    const [error, setError] = useState(null);
+    const {size, gameCount} = useContext(GameContext);
     // size will use later to get best results by size
 
     useEffect(() => {
         async function fetchBestResult() {
-            const response = await fetch('http://fin.local/rest/V1/digits/best');
-            const resData = await response.json();
-            setBestResult(resData);
+            try {
+                const response = await fetch('http://fin.local/rest/V1/digits/best');
+                const resData = await response.json();
+
+                if (!response.ok) {
+                    throw Error('Failed to fetch best results');
+                }
+
+                setBestResult(resData);
+            } catch (error) {
+                setError({message: error.message || 'Could not fetch best results. Please try again.'});
+            }
         }
 
         fetchBestResult();
+
     }, [gameCount, size])
+
+    if (error) {
+        return <ErrorBlock title='An error occured!' message={error.message}/>
+    }
+
     return (
         <>
             <div className="best-results">
                 <p><strong> Best results: </strong></p>
-                {bestResult.map((result)=>
-                    <p key={Math.random()*1000}>
+                {bestResult.map((result) =>
+                    <p key={Math.random() * 1000}>
                         {`Score - ${result.hits} with time - ${result.time}`}
                     </p>
                 )}
             </div>
         </>
-    )
+    );
 }
